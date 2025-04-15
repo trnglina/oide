659ce7a6184aaf591e44982a37c7166aed19c09c
Teng Liu <teng@trnglina.org>
2025-04-16 07:07:57 +0900

    Suppress systemUiVisibility deprecation

--- app/src/main/java/app/oide/MainActivity.kt
+++ app/src/main/java/app/oide/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        @Suppress("DEPRECATION")
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_LOW_PROFILE)
